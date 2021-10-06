62e647b2a6e766baacfa17bd914fe2bc56b5a581
허은실 <zindala@naver.com>
2021-10-06 13:13:32 +0900

    1194 달이 차오른다, 가자

--- Test_1194.java
+++ Test_1194.java
@@ -99,3 +99,99 @@ public class Test_1194 {
 		}
 	}
 }
+// 비트마스크를 이용하여 3차원 visit 배열로 해결
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 백준 달이 차오른다, 가자 : 부분집합 경우의 수를 비트열로 바라보자 어떤 key가 선택되었는지 여부 확인 비트마스크
+public class Main1194 {
+
+	static int n, m; // 미로의 세로 가로 
+	static char[][] map; // 미로
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		map = new char[n][m];
+		Pair start = null;
+		for(int i = 0; i < n; i++) {
+			String input = br.readLine();
+			for(int j = 0; j < m; j++) {
+				map[i][j] = input.charAt(j);
+				if(map[i][j] == '0')
+					start = new Pair(i, j, 0);
+			}
+		}
+		
+		System.out.println(bfs(start));
+	}
+	
+	public static int bfs(Pair start) {
+		Queue<Pair> q = new LinkedList<Pair>();
+		q.add(start);
+		boolean[][][] visit = new boolean[64][n][m];
+		visit[0][start.r][start.c] = true;
+		int sec = -1; // 미로를 탈출하는 최소 이동 횟수
+		
+		while(!q.isEmpty()) {
+			Pair now = q.poll();
+			
+			if(map[now.r][now.c] == '1') {
+				sec = now.sec;
+				break;
+			}
+			
+			for(int i = 0; i < 4; i++) {
+				int nr = now.r + dr[i];
+				int nc = now.c + dc[i];
+				int nkey =  now.keys;
+				
+				if(nr < 0 || nr >= n || nc < 0 || nc >= m || map[nr][nc] == '#') // 범위를 벗어나거나 벽이라면 
+					continue;
+				
+				if(map[nr][nc] >= 'a' && map[nr][nc] <= 'f') { // 열쇠가 있다면
+					nkey = nkey | 1 << map[nr][nc] - 'a';
+				} else if(map[nr][nc] >= 'A' && map[nr][nc] <= 'F') { // 문이 있다면
+					if((nkey & 1 << (map[nr][nc] - 'A')) == 0) // 해당 문에 맞는 열쇠가 없다면
+						continue;
+				}
+				
+				if(visit[nkey][nr][nc]) // 이미 방문했다면
+					continue;
+				
+				visit[nkey][nr][nc] = true;
+				Pair next = new Pair(nr, nc, now.sec + 1);
+				next.keys = nkey;
+				q.add(next);
+			}
+		}
+		
+		return sec;
+	}
+	
+	public static class Pair{
+		int keys;
+		int r;
+		int c;
+		int sec;
+		
+		public Pair(int r, int c, int sec) {
+			this.r = r;
+			this.c = c;
+			this.sec = sec;
+		}
+	}
+
+}
+ */
