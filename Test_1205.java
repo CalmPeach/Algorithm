223cf395b4a1276ce67ce9a1e9279f6573005810
허은실 <zindala@naver.com>
2021-10-09 01:40:28 +0900

    1205 등수 구하기

--- Test_1205.java
+++ Test_1205.java
@@ -1,53 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Test_1205{
-	
-	public static void main(String[] args) throws IOException {
+class Test_1205 {
+
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken()); // n개의 점수
+		int score = Integer.parseInt(st.nextToken()); // 송유진의 새로운 점수
+		int p = Integer.parseInt(st.nextToken());
 		
-		int[] memory = new int[n + 1];
-		int[] price = new int[n + 1];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= n; i++) {
-			memory[i] = Integer.parseInt(st.nextToken());
+		if(n != 0) {
+			st = new StringTokenizer(br.readLine());
+		}
+		int[] scores = new int[n]; // 정수
+		
+		for (int i = 0; i < n; i++)
+			scores[i] = Integer.parseInt(st.nextToken());
+		
+		int minIdx = 0;
+		for(minIdx = 0; minIdx < n; minIdx++) {
+			if(scores[minIdx] < score)
+				break;
 		}
 		
-		st = new StringTokenizer(br.readLine());
-		int min = Integer.MAX_VALUE;
-		for(int i = 1; i <= n; i++) {
-			price[i] = Integer.parseInt(st.nextToken());
-			if(memory[i] >= m && min > price[i])
-				min = price[i];
+		int maxIdx = minIdx - 1;
+		while(maxIdx >= 0 && scores[maxIdx] <= score)
+			maxIdx--;
+		
+		int rank = maxIdx + 1;
+		int cnt = 0;
+		if(minIdx - maxIdx != 1) {
+			cnt = minIdx - maxIdx - 1;
+		} 
+		
+		if(rank + cnt < p) {
+			System.out.println(rank + 1);
+		} else {
+			System.out.println(-1);
 		}
-		
-		int[] dp = new int[m + 1];
-		Arrays.fill(dp, -1);
-		dp[0] = 0;
-		dp[m] = min == Integer.MAX_VALUE? -1: min;
-		
-		for(int i = 1; i <= n; i++) {
-			if(memory[i] >= m)
-				continue;
-			for(int j = m; j >= memory[i]; j--) {
-				if(dp[j] != -1 && j + memory[i] > m) {
-					if(dp[m] == -1 || dp[m] > dp[j] + price[i])
-						dp[m] = dp[j] + price[i];
-				}
-				
-				if(dp[j] != -1 && dp[j - memory[i]] != -1) {
-					dp[j] = Integer.min(dp[j], dp[j - memory[i]] + price[i]);
-				} else if(dp[j - memory[i]] != -1){
-					dp[j] = dp[j - memory[i]] + price[i];
-				}
-			}
-		}
-		
-		System.out.println(dp[m]);
 	}
 }
